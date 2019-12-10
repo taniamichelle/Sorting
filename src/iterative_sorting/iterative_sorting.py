81dbf8a79c79ed7639d993805b465897668f0912
tania <tanislavens@yahoo.com>
2019-12-09 20:41:44 -0500

    completed selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,19 +1,19 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
+    # print('arr', arr)
+    # loop through n-1 elements: 
+    for i in range(0, len(arr)): 
+        cur_index = i 
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
-
-
-
+        for j in range(i+1, len(arr)): 
+            # TO-DO: find next smallest element. (hint, can do in 3 loc) 
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
         # TO-DO: swap
-
-
-
+        print('arr[j]', arr[j], 'min', arr[smallest_index])
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]        
+        print('new arr[j]', arr[j])
+        print('new arr', arr)
 
     return arr
 
@@ -28,3 +28,16 @@ def bubble_sort( arr ):
 def count_sort( arr, maximum=-1 ):
 
     return arr
+
+
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+selection_sort(arr1)
+
+
+#INSERTION SORT pseudocode:
+# for i: 1 to length(A) -1
+#   j=i
+#   while j>0 and A[j-1]>A[j]
+#       swap A[j] and A[j-1]
+#       j = j-1
