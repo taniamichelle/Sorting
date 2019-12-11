3aff8ea418fb3f585d8f38acd6b2a79d4af1b9c5
tania <tanislavens@yahoo.com>
2019-12-10 23:40:25 -0500

    mvp

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -7,34 +7,63 @@ def merge( arrA, arrB ):
     # print(merged_arr)
     # # TO-DO
     while len(arrA) != 0 and len(arrB) != 0:
-        print('arrA: ', arrA, 'arrB: ', arrB)
+        # print('arrA: ', arrA, 'arrB: ', arrB)
         if arrA[0] >= arrB[0]:
             merged_arr.append(arrB[0]) # can combine these 2 lines: merged_arr.append( arrB.pop(0) )
             arrB.pop(0) 
         elif arrA[0] < arrB[0]:
             merged_arr.append(arrA[0])
             arrA.pop(0) 
-        print('merged: ', merged_arr)
+        # print('merged: ', merged_arr)
     if len(arrA) != 0:
         merged_arr.extend(arrA)
     elif len(arrB) != 0:
         merged_arr.extend(arrB)
     
-    print('merged_array:', merged_arr, 'arrB: ', arrB, 'arrA: ', arrA)
+    # print('merged_array:', merged_arr, 'arrB: ', arrB, 'arrA: ', arrA)
 
     return merged_arr
     # print('merged array: ', merged_arr)
 
-arr1 = [0, 1, 4, 9]
-arr4 = [0, 1, 2, 3, 4]
-print(merge(arr1, arr4))
-
+arra = [0, 1, 4, 9]
+arrb = [0, 1, 2, 3, 4]
+# print(merge(arra, arrb))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+# RECURSIVE MERGE SORT PSEUDOCODE TO HALVE ARRAYS:
+# mergesort(array a)
+#     if(n == 1)  # base case
+#         return a
 
-    return arr
+# left = a[0] ... a[n/2]  # Left side of mid-point
+# right = a[n/2 +1] ... a[n]  # Right side of mid-point
+
+# right = mergesort(right)
+# left = mergesort(left)
+
+# return merge(left, right)
+
+def merge_sort( arr ):  # arr has length of 4
+    # TO-DO
+    if len(arr) <= 1:  # base case
+        print('arr: ', arr)
+        return arr
+
+    middle = len(arr) // 2  # middle point is at arr[2]
+    left = arr[:middle]
+    right = arr[middle:]
+    
+    # recursion
+    l = merge_sort(left)
+    r = merge_sort(right)
+
+    # merge the left and right arrays
+    result = merge(l, r)
+    print('result: ', result)
+    return result
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+merge_sort(arr1)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -55,18 +84,7 @@ def timsort( arr ):
 
     return arr
 
-# RECURSIVE MERGE SORT PSEUDOCODE TO HALVE ARRAYS:
-# mergesort(array a)
-#     if(n == 1)  # base case
-#         return a
 
-# arrayOne = a[0] ... a[n/2]  # Left side of mid-point
-# arrayTwo = a[n/2 +1] ... a[n]  # Right side of mid-point
-
-# arrayOne = mergesort(arrayOne)
-# arrayTwo = mergesort(arrayTwo)
-
-# return merge(arrayOne, arrayTwo)
 
 # RECURSIVE MERGE SORT PSEUDOCODE TO RECOMBINE ARRAYS:
 # merge(array a, array b)
@@ -103,12 +121,3 @@ def timsort( arr ):
     #     if len(arrA[0]) > len(arrB[0]):
     #         merged_arr[i] = arrA[i]
 
-
-'''
-1. define a function that takes in an array
-2. split arrary in half
-3. split those resulting arrays in half. continue until resulting arrays can no longer be halved
-4. once you've reached base condition, merge sorted lists
-
-def bear_sorter(arr):
-'''
